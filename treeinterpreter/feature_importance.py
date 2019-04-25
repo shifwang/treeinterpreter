66cb2ca5905d359a5b9fbb232e2b5ff6c460c3a0
yu wang <wang.yu@berkeley.edu>
2019-04-25 17:45:44 +0000

    add normalized and non-nomalized feature importance

--- treeinterpreter/feature_importance.py
+++ treeinterpreter/feature_importance.py
@@ -1,7 +1,7 @@
 from .treeinterpreter import _predict_tree
 from sklearn.ensemble.forest import _generate_unsampled_indices, _generate_sample_indices
 import numpy as np
-def feature_importance(rf, X, y, type = 'oob'):
+def feature_importance(rf, X, y, type = 'oob', normalized = True):
     n_samples, n_features = X.shape
     if len(y.shape) != 2:
         raise ValueError('y must be 2d array (n_samples, 1) if numerical or (n_samples, n_categories).')
@@ -26,8 +26,8 @@ def feature_importance(rf, X, y, type = 'oob'):
         if len(contributions.shape) == 2:
             contributions = contributions[:,:,np.newaxis]
         tmp =  np.tensordot(y[indices,:], contributions, axes=([0, 1], [0, 2])) 
-        out +=  tmp / sum(tmp)
-        SE += (tmp / sum(tmp)) ** 2
+        out +=  tmp / sum(tmp) if normalized else tmp
+        SE += (tmp / sum(tmp)) ** 2 if normalized else tmp ** 2
     out /= rf.n_estimators
     SE /= rf.n_estimators
     SE = ((SE - out ** 2) / rf.n_estimators) ** .5 
