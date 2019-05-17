eede8fe824158f8490f1f54b42d44579a65b618b
yu wang <wang.yu@berkeley.edu>
2019-05-17 07:42:33 +0000

    add MDA for eregression

--- treeinterpreter/feature_importance.py
+++ treeinterpreter/feature_importance.py
@@ -1,21 +1,21 @@
 from .treeinterpreter import _predict_tree
 from sklearn.ensemble.forest import _generate_unsampled_indices, _generate_sample_indices
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, mean_squared_error
 import numpy as np
-def MDA(rf, X, y, type = 'oob', n_trials = 10):
+def MDA(rf, X, y, type = 'oob', n_trials = 10, metric = 'accuracy'):
     if len(y.shape) != 2:
         raise ValueError('y must be 2d array (n_samples, 1) if numerical or (n_samples, n_categories).')
     n_samples, n_features = X.shape
     fi_mean = np.zeros((n_features,))
     fi_std = np.zeros((n_features,))
-    best_score = rf_accuracy(rf, X, y, type = type)
+    best_score = rf_accuracy(rf, X, y, type = type, metric = metric)
     for f in range(n_features):
         permute_score = 0
         permute_std = 0
         X_permute = X.copy()
         for i in range(n_trials):
             X_permute[:, f] = np.random.permutation(X_permute[:, f])
-            to_add = rf_accuracy(rf, X_permute, y, type = type)
+            to_add = rf_accuracy(rf, X_permute, y, type = type, metric = metric)
             permute_score += to_add
             permute_std += to_add ** 2
         permute_score /= n_trials
@@ -25,15 +25,24 @@ def MDA(rf, X, y, type = 'oob', n_trials = 10):
         fi_std[f] = permute_std
     return fi_mean, fi_std
          
-    
-def rf_accuracy(rf, X, y, type = 'oob'):
+def neg_mse(y, y_hat):
+    return - mean_squared_error(y, y_hat)
+
+def rf_accuracy(rf, X, y, type = 'oob', metric = 'accuracy'):
+    if metric == 'accuracy':
+        score = accuracy_score
+    elif metric == 'mse':
+        score = neg_mse
+    else:
+        raise ValueError('metric type not understood')
+
     n_samples, n_features = X.shape
     tmp = 0
     count = 0
     if type == 'test':
-        return accuracy_score(y, rf.predict(X))
+        return score(y, rf.predict(X))
     elif type == 'train' and not rf.bootstrap:
-        return accuracy_score(y, rf.predict(X))
+        return score(y, rf.predict(X))
 
     for tree in rf.estimators_:
         if type == 'oob':
@@ -45,7 +54,7 @@ def rf_accuracy(rf, X, y, type = 'oob'):
             indices = _generate_sample_indices(tree.random_state, n_samples)
         else:
             raise ValueError('type is not recognized. (%s)'%(type))
-        tmp +=  accuracy_score(y[indices,:], tree.predict(X[indices, :])) * len(indices) 
+        tmp +=  score(y[indices,:], tree.predict(X[indices, :])) * len(indices) 
         count += len(indices)
     return tmp / count
     
